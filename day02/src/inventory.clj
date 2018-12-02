c0c01e17daac539f78292b7dd0b0a079ee71bf58
tdekeyser <tom_dekeyser_7@hotmail.com>
2018-12-02 18:07:54 +0100

    tom: day02: puzzle 2

--- day02/src/inventory.clj
+++ day02/src/inventory.clj
@@ -15,8 +15,11 @@
 (defn occurs-exactly?
   "Returns 1 if any letter appears exactly occ times, else 0."
   [occ message]
-  (let [freq (frequencies (cljstr/split message #""))]
-    (bool->int (some #(= occ (last %)) freq))))
+  (->> (cljstr/split message #"")
+       frequencies
+       (map #(last %))
+       (some #(= occ %))
+       bool->int))
 
 
 (defn inventory-checksum
@@ -30,13 +33,62 @@
         (recur
           (+ twos (occurs-exactly? 2 message))
           (+ threes (occurs-exactly? 3 message))
-          (drop 1 inv))))))
+          (rest inv))))))
+
+;;;;;;;;;;;;;;
+;; Puzzle 2 ;;
+;;;;;;;;;;;;;;
+
+(defn differs-1-char?
+  "Return true if str1 and str2 differ exactly 1 character at same index, else false."
+  [str1 str2]
+  (loop [ch1   (cljstr/split str1 #"")
+         ch2   (cljstr/split str2 #"")
+         diff  0]
+    (cond (> diff 1)    false
+          (empty? ch1)  (= diff 1)
+          :else         (recur
+                          (rest ch1)
+                          (rest ch2)
+                          (if (= (first ch1) (first ch2))
+                            diff
+                            (inc diff))))))
 
 
-(def read-input
+(defn drop-diff
+  "Drop differing chars."
+  [str1 str2]
+  (->> str1
+       (map #(if (= %1 %2) %1 "") str2)
+       (apply str)))
+
+
+(def not-nil? (complement nil?))
+
+
+(defn find-similar
+  [message messageList]
+  (loop [list messageList]
+    (cond (empty? list)                          nil
+          (differs-1-char? message (first list)) (first list)
+          :else                                  (recur (rest list)))))
+
+
+(defn find-similar-for-all
+  [messageList1 messageList2]
+  (loop [list1 messageList1]
+    (let [message        (first list1)
+          similar        (find-similar message messageList2)]
+      (if (not-nil? similar)
+        (drop-diff message similar)
+        (recur (rest list1))))))
+
+
+(def puzzle-input
   (with-open [r (io/reader "input.txt")]
     (doall (line-seq r))))
 
 
 (defn -main []
-  (print "Puzzle 1: Checksum is " (inventory-checksum read-input)))
+  (print "Puzzle 1: Checksum is" (inventory-checksum puzzle-input) "\n")
+  (print "Puzzle 2: Similar chars are" (find-similar-for-all puzzle-input puzzle-input) "\n"))
--- day02/test/inventory_test.clj
+++ day02/test/inventory_test.clj
@@ -23,3 +23,32 @@
            (is
              (= (inventory-checksum '("abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"))
                 (* 4 3)))))
+
+(deftest test-differs-1-char?
+  (testing "Check if a boxId is the correct one (has another that differs exactly 1 character)."
+           (is (= (differs-1-char? "abc" "abd") true))
+           (is (= (differs-1-char? "abc" "abc") false))
+           (is (= (differs-1-char? "abc" "ayc") true))
+           (is (= (differs-1-char? "abc" "rbc") true))
+           (is (= (differs-1-char? "abc" "bca") false))
+           (is (= (differs-1-char? "abc" "bac") false))
+           (is (= (differs-1-char? "acb" "abc") false))
+           (is (= (differs-1-char? "abc" "abg") true))
+           (is (= (differs-1-char? "abc" "atd") false))
+           (is (= (differs-1-char? "abcpoppie" "tbcpoppie") true))))
+
+(deftest test-find-similar
+  (testing "Finds similar string to input"
+           (is (= (find-similar "abc" '("bdhe" "tup" "acc")) "acc"))
+           (is (= (find-similar "abc" '("bdhe" "tup")) nil))
+           (is (= (find-similar "abcd" '("bdhe" "tup" "acc" "abce" "iope")) "abce"))))
+
+(deftest test-drop-diff
+  (testing "Drop differing chars."
+           (is (= (drop-diff "abc" "adc") "ac"))
+           (is (= (drop-diff "patoe" "patie") "pate"))))
+
+(deftest test-find-similar-for-all
+  (testing "Puzzle 2"
+           (is (= (find-similar-for-all '("bdhe" "tup" "acc") '("bdhe" "tup" "acd")) "ac"))
+           (is (= (find-similar-for-all '("bdhe" "tup" "acc" "abce" "iope") '("iope" "bdhe" "tup" "acc" "pbce")) "bce"))))
